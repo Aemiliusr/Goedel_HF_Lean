d7b6af56333f7874b1d7eaa0dcc79790d67835f9
Ruben Reijerse <ruben.reijerse23@imperial.ac.uk>
2024-08-13 14:43:06 +0100

    update

--- GIT.lean
+++ GIT.lean
@@ -1,5 +1,6 @@
 -- This module serves as the root of the `GIT` library.
 -- Import modules here that should be built as part of the library.
+import GIT.Calculus
 import GIT.Basic
 import GIT.Ordinal
 import GIT.Function
